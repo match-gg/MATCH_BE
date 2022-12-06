15c42402bde59baf7f5b194341f88cf77f21df84
jeongas9810@gmail.com <jeongas9810@gmail.com>
2022-12-06 11:13:25 +0900

    :tada: Project start

--- .github/ISSUE_TEMPLATE/refactor-template.md
+++ .github/ISSUE_TEMPLATE/refactor-template.md
@@ -4,9 +4,7 @@ about: 리팩토링용 템플릿
 title: 명사형으로 제목 작성
 labels: ":recycle: recycle"
 assignees: ''
-
 ---
-
 ## 상세 내용
 >
 
---
+++ src/test/kotlin/gg/match/domain/DomainApplicationTests.kt
@@ -0,0 +1,13 @@
+package gg.match.domain
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DomainApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
