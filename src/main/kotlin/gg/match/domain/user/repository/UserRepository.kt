1a7d21d742b23b0ab2e0fe25ff22c7dad99ed25c
jeongas9810@gmail.com <jeongas9810@gmail.com>
2023-02-16 12:23:36 +0900

    :card_file_box: add user entity and base entity setting

---
+++ src/main/kotlin/gg/match/domain/user/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package gg.match.domain.user.repository
+
+import gg.match.domain.user.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository: JpaRepository<User, Long> {
+    fun findByOauth2Id(oauth2Id: String): User?
+    fun existsByOauth2Id(oauth2Id: String): Boolean
+}
