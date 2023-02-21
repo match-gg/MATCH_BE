dbc273373920929cba9a79cf9d775a3cbf26049c
jeongas9810@gmail.com <jeongas9810@gmail.com>
2023-02-22 03:59:45 +0900

    :heavy_plus_sign: Add a dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,11 @@ dependencies {
     implementation("com.coveo:spring-boot-parameter-store-integration:1.1.2")
     //oauth
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    //jwt
+    //jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 }
 
 tasks.withType<KotlinCompile> {
