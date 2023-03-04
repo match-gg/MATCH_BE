0cd617aa1f9178106c69d9116bc8a1799f151bf6
tjdrbdl <kim178400@naver.com>
2023-03-04 16:24:59 +0900

    :sparkles: 엔티티, @enumerated 추가

--- src/main/kotlin/gg/match/domain/board/overwatch/entity/Overwatch.java
+++ src/main/kotlin/gg/match/domain/board/overwatch/entity/Overwatch.java
@@ -12,11 +12,14 @@ public class Overwatch {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String tier;
-    private String position;
+    @Enumerated(EnumType.STRING)
+    private Tier tier;
+    @Enumerated(EnumType.STRING)
+    private Position position;
     private Boolean voice;
     private String content;
-    private String expire;
+    @Enumerated(EnumType.STRING)
+    private Expire expire;
     private Date regdate;
 
     public void setId(Long id) {
@@ -25,5 +28,61 @@ public class Overwatch {
 
     public Long getId() {
         return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setTier(Tier tier) {
+        this.tier = tier;
+    }
+
+    public Tier getTier() {
+        return tier;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setVoice(Boolean voice) {
+        this.voice = voice;
+    }
+
+    public Boolean getVoice() {
+        return voice;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setExpire(Expire expire) {
+        this.expire = expire;
+    }
+
+    public Expire getExpire() {
+        return expire;
+    }
+
+    public void setRegdate(Date regdate) {
+        this.regdate = regdate;
+    }
+
+    public Date getRegdate() {
+        return regdate;
     }
 }
