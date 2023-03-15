a99c1d4746923bff901a46a9a9b2e8ad9d154d94
jeongas9810@gmail.com <jeongas9810@gmail.com>
2023-03-15 17:46:47 +0900

    :sparkles: add custom annotation

---
+++ src/main/kotlin/gg/match/common/annotation/CurrentUser.kt
@@ -0,0 +1,8 @@
+package gg.match.common.annotation
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : user")
+annotation class CurrentUser
