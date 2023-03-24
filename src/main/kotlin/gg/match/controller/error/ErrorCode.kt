bacfdc249e3325b6e2d1b38182d8d9cdeefd4926
jeongas9810@gmail.com <jeongas9810@gmail.com>
2023-03-25 07:44:50 +0900

    :art: Improve Structure

--- src/main/kotlin/gg/match/controller/error/ErrorCode.kt
+++ src/main/kotlin/gg/match/controller/error/ErrorCode.kt
@@ -16,6 +16,8 @@ enum class ErrorCode(
     EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 리프레시 토큰입니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
     LOGOUT_TOKEN(HttpStatus.UNAUTHORIZED, "로그아웃한 회원입니다."),
+    //COMMON
+    NO_BOARD_FOUND(HttpStatus.NOT_FOUND, "게시글이 존재하지 않습니다."),
     //LOL
     USER_NOT_RANKED(HttpStatus.NOT_FOUND, "아직 랭크정보를 보유하고 있지 않습니다.")
 }
