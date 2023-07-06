6da44c7b4fdf98445a640f9313c07a17cd2e7ba2
jeongas9810@gmail.com <jeongas9810@gmail.com>
2023-07-06 20:57:59 +0900

    :sparkles: add chatService by overwatch

--- src/main/kotlin/gg/match/domain/board/overwatch/repository/HeroRepository.kt
+++ src/main/kotlin/gg/match/domain/board/overwatch/repository/HeroRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface HeroRepository: JpaRepository<Hero, Long> {
     fun findByNameAndBattletagAndType(name: String, battletag: Int, type: Type): Hero
+    fun findByName(name: String): Hero
 }
