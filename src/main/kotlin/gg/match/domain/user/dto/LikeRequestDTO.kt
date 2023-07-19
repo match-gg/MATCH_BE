448360e20501bdb05bec52a1d6804ccaa2a4c6c6
jeongas9810@gmail.com <jeongas9810@gmail.com>
2023-07-19 16:50:34 +0900

    add jsonproperty

--- src/main/kotlin/gg/match/domain/user/dto/LikeRequestDTO.kt
+++ src/main/kotlin/gg/match/domain/user/dto/LikeRequestDTO.kt
@@ -1,6 +1,11 @@
 package gg.match.domain.user.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class LikeRequestDTO (
+    @JsonProperty("game")
     val game: String,
+
+    @JsonProperty("nickname")
     val nickname: String
 )
