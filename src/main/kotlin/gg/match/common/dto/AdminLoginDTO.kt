3d88e96a1a0d9aa65ae533aa442088e05a0fe441
jeongas9810@gmail.com <jeongas9810@gmail.com>
2023-08-30 15:41:55 +0900

    .

--- src/main/kotlin/gg/match/common/dto/AdminLoginDTO.kt
+++ src/main/kotlin/gg/match/common/dto/AdminLoginDTO.kt
@@ -1,6 +1,11 @@
 package gg.match.common.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class AdminLoginDTO (
+    @JsonProperty("id")
     var id: String,
+    
+    @JsonProperty("pw")
     var pw: String
 )
